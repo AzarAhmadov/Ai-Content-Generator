98abcd1fb950b8f6eb18c24b9948056c47ca12cc
AzarAhmadov <ehmedovh111@gmail.com>
2024-07-19 00:01:56 +0400

    Create constants.ts

---
+++ src/constants/constants.ts
@@ -0,0 +1,24 @@
+import { Home, History, WalletMinimal, SlidersHorizontal } from 'lucide-react'
+
+export const NavLink = [
+    {
+        title: "Home",
+        icon: Home,
+        path: "/dashboard"
+    },
+    {
+        title: "History",
+        icon: History,
+        path: "/dashboard/history"
+    },
+    {
+        title: "Billing",
+        icon: WalletMinimal,
+        path: "/dashboard/billing"
+    },
+    {
+        title: "Setting",
+        icon: SlidersHorizontal,
+        path: "/dashboard/setting"
+    },
+]
