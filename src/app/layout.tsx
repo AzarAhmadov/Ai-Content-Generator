22e895aa0f117f638779e63ac78be10cd3d60804
AzarAhmadov <ehmedovh111@gmail.com>
2024-07-19 00:01:54 +0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,17 @@
 import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
+import { Poppins } from 'next/font/google'
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+const roboto = Poppins({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '700'],
+})
+
+export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>
+        <body className={roboto.className}>
           <main>
             {children}
           </main>
