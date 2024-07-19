b3eb42ddbb229e095cc060c9c6531542e3df0491
AzarAhmadov <ehmedovh111@gmail.com>
2024-07-20 00:34:49 +0400

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Search from './(components)/_search/Search'
+import ServicesList from './(components)/_servicesList/ServicesList'
 
 const page = () => {
     return (
         <>
             <Search />
+            <ServicesList />
         </>
     )
 }
