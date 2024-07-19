3cd277f4089d4170069572f6bdb111f67e28d20d
AzarAhmadov <ehmedovh111@gmail.com>
2024-07-19 13:05:32 +0400

    Update layout.tsx

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,20 @@
+import Aside from "./(components)/_aside/Aside";
+import Header from "./(components)/_header/Header";
+
 export default function layout({ children, }: Readonly<{ children: React.ReactNode; }>) {
     return (
         <>
-            {children}
+            <div className='flex h-[200vh]'>
+                <Aside />
+                <div className='flex flex-col flex-1'>
+                    <div className='flex flex-col border-r border-[#00000017] w-full'>
+                        <Header />
+                    </div>
+                    <div className='px-[1.5rem] pt-[20px]'>
+                        {children}
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
