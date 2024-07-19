859cf18a0c7053fee6723fdf998df3e7ea3ff3ec
AzarAhmadov <ehmedovh111@gmail.com>
2024-07-20 00:26:58 +0400

    add cdn-icons-png.flaticon.com domain name for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+            "cdn-icons-png.flaticon.com",
+        ],
+    },
+};
 
 export default nextConfig;
