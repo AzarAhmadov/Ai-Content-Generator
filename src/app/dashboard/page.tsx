b4450dc7ca4aadd9e6b0c93348d3761f21d5ebcc
AzarAhmadov <ehmedovh111@gmail.com>
2024-07-19 13:05:37 +0400

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,21 +1,10 @@
 import React from 'react'
-import Header from './(components)/_header/Header'
-import Aside from './(components)/_aside/Aside'
+import Search from './(components)/_search/Search'
 
 const page = () => {
     return (
         <>
-            <div className='flex h-[200vh]'>
-                <Aside />
-                <div className='flex flex-col flex-1'>
-                    <div className='flex flex-col border-r border-[#00000017] w-full'>
-                        <Header />
-                    </div>
-                    <div className='px-[1.5rem] pt-[20px]'>
-
-                    </div>
-                </div>
-            </div>
+            <Search />
         </>
     )
 }
