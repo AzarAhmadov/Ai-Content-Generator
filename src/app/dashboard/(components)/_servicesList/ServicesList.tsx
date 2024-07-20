5542bed4252f55c7397d750664b136ec17fd5edb
AzarAhmadov <ehmedovh111@gmail.com>
2024-07-20 13:38:50 +0400

    Search
    Added Search Functionality

---
+++ src/app/dashboard/(components)/_servicesList/ServicesList.tsx
@@ -0,0 +1,41 @@
+import React, { FC, useEffect, useState } from 'react'
+import ServicesCards from '../_servicesCards/ServicesCards'
+import { Templates } from '@/constants/constants'
+import { ISearch, IServices } from '@/types/type'
+import Image from 'next/image'
+
+const ServicesList: FC<ISearch> = ({ search }) => {
+
+    const [filteredTemplates, setFilteredTemplates] = useState(Templates)
+
+    useEffect(() => {
+        if (search) {
+            const filterTemplates = Templates.filter((item) => item.name.toLowerCase().trim().includes(search.toLowerCase().trim()))
+            setFilteredTemplates(filterTemplates)
+        } else {
+            setFilteredTemplates(Templates)
+        }
+    }, [search])
+
+    return (
+        <>
+            {
+                filteredTemplates.length === 0 ?
+                    <div className='flex justify-center pt-10 flex-col items-center gap-4'>
+                        <Image src={'https://cdn-icons-png.flaticon.com/128/6741/6741076.png'} width={100} height={0} alt='No Result' />
+                        <p className='text-[17px] text-pink-800'>No services found matching your search criteria.</p>
+                    </div>
+                    :
+                    <div className='grid grid-cols-4 gap-4 mt-4'>
+                        {
+                            filteredTemplates.map((item: IServices, index: number) => (
+                                <ServicesCards {...item} key={index} />
+                            ))
+                        }
+                    </div>
+            }
+        </>
+    )
+}
+
+export default ServicesList
