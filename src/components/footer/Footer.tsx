2352619c4905806cccf9425fe7929ce5bd184bed
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-02 10:46:21 +0400

    Update Footer.tsx

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer className='text-center py-3 text-[17px]'>
+        <footer className='text-center py-3 text-[16px] md:text-[17px]'>
             Created By <Link className='border-b-2 hover:text-blue-600 transition-all border-blue-600' target='_blank' href={'https://azarahmadov.vercel.app/'}>Azar Ahmadov</Link>
         </footer>
     )
