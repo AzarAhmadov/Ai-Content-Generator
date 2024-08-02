eb5365b6e50ce930f094ed959167f2ff5a60fd92
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-02 10:46:18 +0400

    Update Header.tsx

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,13 +5,13 @@ import React from 'react'
 const Header = () => {
     return (
         <header>
-            <div className="container px-14 py-5 border-b flex items-center justify-between">
+            <div className="container px-4 md:px-14 py-5 border-b flex items-center justify-between">
                 <Link className='flex justify-center items-center gap-2' href={'/'}>
-                    <img src="svgs/logo.svg" alt="Content AI" />
-                    <h3 className='text-xl'> Content AI </h3>
+                    <img className='w-[28px] md:w-[40px]' src="svgs/logo.svg" alt="Content AI" />
+                    <span className='md:text-xl text-[16px]'> Content AI </span>
                 </Link>
-                <Link href={'/sign-up'} className='flex items-center gap-2 text-[17px] font-[400]'>
-                    <User />
+                <Link href={'/sign-up'} className='flex items-center gap-2 text-[15px] md:text-[18px] font-[400]'>
+                    <User className='w-[20px] md:w-[28px]' />
                     Get Started
                 </Link>
             </div>
