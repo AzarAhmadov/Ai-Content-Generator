4c9e1e345375c620caa5ccac6bb406bcff63f169
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-13 15:25:07 +0400

    Update Header.tsx

--- src/app/dashboard/(components)/_header/Header.tsx
+++ src/app/dashboard/(components)/_header/Header.tsx
@@ -1,20 +1,41 @@
+'use client'
+
 import { Search } from 'lucide-react'
+import dynamic from 'next/dynamic'
+import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
+const AsideMobile = dynamic(() => import('../_asideMobilMenu/AsideMobile'), { ssr: false })
 
 const Header = () => {
+
+    const [mobile, setMobile] = useState(false)
+
+    const openMenu = () => {
+        setMobile(!mobile)
+    }
+
     return (
-        <header className='border-b border-[#00000017]'>
-            <div className="container px-[20px] h-[76px] flex items-center justify-between">
-                <form className='flex items-center justify-between border rounded-md w-[400px] pe-3'>
-                    <input className='w-full bg-transparent p-[12px] outline-none' type="text" placeholder='Search...' />
-                    <Search />
-                </form>
-                <Link href={''} className='hover:scale-105 transition-all text-sm rounded-full bg-gradient-to-tr from-blue-600 to-blue-400 text-white px-3 py-2'>
-                    ✨ Join Membership just for $99/Month
-                </Link>
-            </div>
-        </header>
+        <>
+            <header className='border-b border-[#00000017]'>
+                <div className="container gap-5 px-[20px] h-[76px] flex items-center justify-between">
+                    <form className='flex items-center justify-between border rounded-md w-full max-w-[400px] pe-3'>
+                        <input className='w-full bg-transparent p-[10px] md:p-[12px] outline-none' type="text" placeholder='Search...' />
+                        <Search />
+                    </form>
+                    <Link href={''} className='hover:scale-105 md:block hidden transition-all text-sm rounded-full bg-gradient-to-tr from-blue-600 to-blue-400 text-white px-3 py-2'>
+                        ✨ Join Membership just for $99/Month
+                    </Link>
+                    <button onClick={openMenu} className='md:hidden block'>
+                        <Image width={30} height={30} src="https://cdn-icons-png.flaticon.com/128/8917/8917404.png" alt='menu_bar' />
+                    </button>
+                </div>
+            </header>
+
+            {
+                <AsideMobile mobile={mobile} />
+            }
+        </>
     )
 }
 
