46a343d1a3044fd3e97b81e2212f4765bb2ece26
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-13 15:25:42 +0400

    Update ServicesCards.tsx

--- src/app/dashboard/(components)/_servicesCards/ServicesCards.tsx
+++ src/app/dashboard/(components)/_servicesCards/ServicesCards.tsx
@@ -1,22 +1,22 @@
 import { IServices } from '@/types/type'
 import Image from 'next/image'
-import React, { FC } from 'react'
+import React, { FC, memo } from 'react'
 
 const ServicesCards: FC<IServices> = (item) => {
 
     return (
-        <div className='border flex flex-col justify-between p-5 rounded-md shadow-sm cursor-pointer hover:scale-105 transition-all'>
+        <div className='border flex flex-col justify-between p-3 md:p-5 rounded-md shadow-sm cursor-pointer hover:scale-105 transition-all'>
             <div>
                 <Image src={item.icon} width={50} height={50} alt={item.name} />
-                <h3 className='py-3 font-bold text-[18px]'>
+                <h3 className='py-3 font-bold text-[14px] md:text-[18px]'>
                     {item.name}
                 </h3>
             </div>
-            <p className='line-clamp-2 text-[14px] text-[#00000094]'>
+            <p className='line-clamp-2 text-[12px] md:text-[14px] text-[#00000094]'>
                 {item.desc}
             </p>
         </div>
     )
 }
 
-export default ServicesCards
+export default memo(ServicesCards)
