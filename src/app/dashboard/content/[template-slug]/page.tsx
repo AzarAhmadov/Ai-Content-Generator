033646de3fa278b201a8e4d8167f1b4a2d2d7835
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-15 15:19:06 +0400

    Update page.tsx

--- src/app/dashboard/content/[template-slug]/page.tsx
+++ src/app/dashboard/content/[template-slug]/page.tsx
@@ -1,13 +1,23 @@
-'use client'
+import { IServices } from '@/types/type'
+import React, { FC } from 'react'
+import FormArea from '../_components/formArea/FormArea'
+import OutputArea from '../_components/outputArea/OutputArea'
+import { Templates } from '@/constants/constants'
 
-import React from 'react'
+interface Iparams {
+    params: {
+        'template-slug': string
+    }
+}
 
-const page = ({ params }) => {
+const page: FC<Iparams> = (props: Iparams) => {
 
+    const selectedTemplate: IServices | any = Templates.find((item) => item.slug == props.params['template-slug'])
 
     return (
-        <div>
-            New Content Area
+        <div className='grid grid-cols-1 md:grid-cols-2'>
+            <FormArea selectedTemplate={selectedTemplate} />
+            <OutputArea />
         </div>
     )
 }
