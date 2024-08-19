67cda46e5f9a187bc398f00d52834a5714a7f601
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-19 15:14:50 +0400

    Update ServicesList.tsx

--- src/app/dashboard/(components)/servicesList/ServicesList.tsx
+++ src/app/dashboard/(components)/servicesList/ServicesList.tsx
@@ -15,23 +15,30 @@ const ServicesList: FC<ISearch> = ({ search }) => {
         return Templates
     }, [search])
 
+    const renderFilter = () => {
+        if (filteredTemplates.length === 0) {
+            return (
+                <div className='flex justify-center pt-10 flex-col text-center items-center gap-4'>
+                    <Image src={'https://cdn-icons-png.flaticon.com/128/6741/6741076.png'} width={100} height={0} alt='No Result' />
+                    <p className='text-[17px] text-pink-800'>No services found matching your search criteria.</p>
+                </div>
+            )
+        } else {
+            return (
+                <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 mt-4'>
+                    {
+                        filteredTemplates.map((item: IServices, index: number) => (
+                            <ServicesCards {...item} key={index} />
+                        ))
+                    }
+                </div>
+            )
+        }
+    }
+
     return (
         <>
-            {
-                filteredTemplates.length === 0 ?
-                    <div className='flex justify-center pt-10 flex-col text-center items-center gap-4'>
-                        <Image src={'https://cdn-icons-png.flaticon.com/128/6741/6741076.png'} width={100} height={0} alt='No Result' />
-                        <p className='text-[17px] text-pink-800'>No services found matching your search criteria.</p>
-                    </div>
-                    :
-                    <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 mt-4'>
-                        {
-                            filteredTemplates.map((item: IServices, index: number) => (
-                                <ServicesCards {...item} key={index} />
-                            ))
-                        }
-                    </div>
-            }
+            {renderFilter()}
         </>
     )
 }
