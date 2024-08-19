244c41b7808797256bcf5a45a3af45611ad6cf80
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-19 15:14:53 +0400

    Update Header.tsx

--- src/app/dashboard/(components)/header/Header.tsx
+++ src/app/dashboard/(components)/header/Header.tsx
@@ -5,18 +5,34 @@ import { Search } from 'lucide-react';
 import dynamic from 'next/dynamic';
 import Image from 'next/image';
 import Link from 'next/link';
-import React, { useEffect } from 'react';
-
+import React from 'react';
 const AsideMobile = dynamic(() => import('../asideMobilMenu/AsideMobile'), { ssr: false });
 
 const Header = () => {
 
     const [toggle, handleToggle] = useToggle(false);
 
+    const renderToggle = () => {
+        return (
+            <>
+                {
+                    toggle ?
+                        <button onClick={handleToggle} className='lg:hidden block'>
+                            <Image width={30} height={30} src="https://cdn-icons-png.flaticon.com/128/1828/1828665.png" alt='close_bar' />
+                        </button>
+                        :
+                        <button onClick={handleToggle} className='lg:hidden block'>
+                            <Image width={30} height={30} src="https://cdn-icons-png.flaticon.com/128/8917/8917404.png" alt='menu_bar' />
+                        </button>
+                }
+            </>
+        )
+    }
+
     return (
         <>
             <header className='border-b border-[#00000017]'>
-                <div className="container gap-5 px-[20px] h-[76px] flex items-center justify-between">
+                <div className="container gap-5 px-[13px] md:px-[20px] h-[76px] flex items-center justify-between">
                     <form className='flex items-center justify-between border rounded-md w-full max-w-[400px] pe-3'>
                         <input className='w-full bg-transparent p-[10px] md:p-[12px] outline-none' type="text" placeholder='Search...' />
                         <Search />
@@ -25,16 +41,7 @@ const Header = () => {
                         ✨ Join Membership just for $99/Month
                     </Link>
 
-                    {
-                        toggle ?
-                            <button onClick={handleToggle} className='lg:hidden block'>
-                                <Image width={30} height={30} src="https://cdn-icons-png.flaticon.com/128/1828/1828665.png" alt='close_bar' />
-                            </button>
-                            :
-                            <button onClick={handleToggle} className='lg:hidden block'>
-                                <Image width={30} height={30} src="https://cdn-icons-png.flaticon.com/128/8917/8917404.png" alt='menu_bar' />
-                            </button>
-                    }
+                    {renderToggle()}
                 </div>
             </header>
 
