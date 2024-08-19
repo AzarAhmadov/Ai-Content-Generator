6b918b5a06a953b271400eb71706ddc22f2148b8
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-19 18:01:18 +0400

    Update page.tsx

--- src/app/dashboard/content/[template-slug]/page.tsx
+++ src/app/dashboard/content/[template-slug]/page.tsx
@@ -1,8 +1,12 @@
+'use client'
+
 import { IServices } from '@/types/type'
 import React, { FC } from 'react'
 import FormArea from '../_components/formArea/FormArea'
 import OutputArea from '../_components/outputArea/OutputArea'
 import { Templates } from '@/constants/constants'
+import Link from 'next/link'
+import { ArrowLeft } from 'lucide-react'
 
 interface Iparams {
     params: {
@@ -15,9 +19,17 @@ const page: FC<Iparams> = (props: Iparams) => {
     const selectedTemplate: IServices | any = Templates.find((item) => item.slug == props.params['template-slug'])
 
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2'>
-            <FormArea selectedTemplate={selectedTemplate} />
-            <OutputArea />
+        <div>
+            <Link className='flex gap-1 bg-gray-400 w-max py-2 px-3 text-white rounded-md mb-5' href={'/dashboard'}>
+                <ArrowLeft />  Back
+            </Link>
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-x-0 md:gap-x-6'>
+                <FormArea userFormInput={(e: any) => console.log(e)} selectedTemplate={selectedTemplate} />
+
+                <div className='col-span-2 bg-red-400'>
+                    <OutputArea />
+                </div>
+            </div>
         </div>
     )
 }
