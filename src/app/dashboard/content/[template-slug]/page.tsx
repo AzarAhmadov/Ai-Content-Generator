986c705affce54470629fdd14aaca68fbda66361
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-19 23:04:56 +0400

    Update page.tsx

--- src/app/dashboard/content/[template-slug]/page.tsx
+++ src/app/dashboard/content/[template-slug]/page.tsx
@@ -18,15 +18,19 @@ const page: FC<Iparams> = (props: Iparams) => {
 
     const selectedTemplate: IServices | any = Templates.find((item) => item.slug == props.params['template-slug'])
 
+    const GenerateAIContent = (formData: any) => {
+     
+    }
+
     return (
         <div>
             <Link className='flex gap-1 bg-gray-400 w-max py-2 px-3 text-white rounded-md mb-5' href={'/dashboard'}>
                 <ArrowLeft />  Back
             </Link>
-            <div className='grid grid-cols-1 md:grid-cols-3 gap-x-0 md:gap-x-6'>
-                <FormArea userFormInput={(e: any) => console.log(e)} selectedTemplate={selectedTemplate} />
+            <div className='grid grid-cols-1 gap-y-7 md:grid-cols-3 gap-x-0 md:gap-x-5'>
+                <FormArea userFormInput={(e: any) => GenerateAIContent(e)} selectedTemplate={selectedTemplate} />
 
-                <div className='col-span-2 bg-red-400'>
+                <div className='col-span-2'>
                     <OutputArea />
                 </div>
             </div>
