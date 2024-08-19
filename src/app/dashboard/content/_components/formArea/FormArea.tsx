b1a906f8bb40a5f60126c4b61414ab6dcef97cd8
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-19 15:14:47 +0400

    Update FormArea.tsx

--- src/app/dashboard/content/_components/formArea/FormArea.tsx
+++ src/app/dashboard/content/_components/formArea/FormArea.tsx
@@ -1,6 +1,7 @@
-'use client'
-
+import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
 import { IServices } from '@/types/type'
+import Image from 'next/image'
 import React from 'react'
 
 interface Props {
@@ -9,9 +10,45 @@ interface Props {
 
 const FormArea: React.FC<Props> = ({ selectedTemplate }) => {
 
+    const { icon, desc, name, form } = selectedTemplate
+
+    const InputArea = (input: any) => {
+        if (input.field == 'input') {
+            return (
+                <div className='mb-6'>
+                    <Input />
+                </div>
+            )
+        } else if (input.field == 'textarea') {
+            return (
+                <div>
+                    <Textarea />
+                </div>
+            )
+        } else {
+            return null
+        }
+    }
+
     return (
         <div>
-            {selectedTemplate.name}
+            <div className='space-y-4'>
+                <Image src={icon} alt={selectedTemplate.name} width={60} height={60} />
+                <h3 className='font-bold text-[25px] text-[#2980b9]'>{name}</h3>
+                <p className='text-[#000000d7]'>{desc}</p>
+            </div>
+            <form className='mt-8'>
+                {
+                    form.map((el, index) => (
+                        <div key={index}>
+                            <label className='mb-3 flex font-[500]'> {el.label} </label>
+                            {
+                                InputArea(el)
+                            }
+                        </div>
+                    ))
+                }
+            </form>
         </div>
     )
 }
