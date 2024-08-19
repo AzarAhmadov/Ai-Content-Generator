9a5b78f17958c61b70c9637d395898d9b734fe8b
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-19 15:14:59 +0400

    Update Aside.tsx

--- src/app/dashboard/(components)/aside/Aside.tsx
+++ src/app/dashboard/(components)/aside/Aside.tsx
@@ -1,7 +1,7 @@
 'use client'
 
+import Logo from '@/components/common/logo/Logo'
 import { NavLink } from '@/constants/constants'
-import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
@@ -13,13 +13,7 @@ const Aside = () => {
     return (
         <aside className='min-w-[270px] lg:flex hidden'>
             <div className='fixed min-w-[270px] border-r border-[#00000017] h-screen z-10'>
-                <Link href="/" className='border-b items-center gap-3 border-b-[#00000017] flex justify-center h-[77px]'>
-                    <Image src={'/svgs/logo.svg'} width={50} height={50} alt='Logo' />
-                    <h3 className='text-[20px] font-medium bg-gradient-to-tr from-blue-700 to-blue-400 bg-clip-text text-transparent'>
-                        Content AI
-                    </h3>
-                </Link>
-
+                <Logo />
                 <nav className='p-5'>
                     <ul className='flex flex-col gap-3'>
                         {NavLink.map((menu, index) => (
