4ce682cee91b998b2a9e68d1c3f01e3d49463086
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-19 15:14:27 +0400

    Update Header.tsx

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,17 +1,22 @@
 import { User } from 'lucide-react'
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
     return (
         <header>
-            <div className="container px-4 md:px-14 py-5 border-b flex items-center justify-between">
+            <div className="container px-4 md:px-14 py-3 md:py-5 border-b flex items-center justify-between">
                 <Link className='flex justify-center items-center gap-2' href={'/'}>
-                    <img className='w-[28px] md:w-[40px]' src="svgs/logo.svg" alt="Content AI" />
-                    <span className='md:text-xl text-[16px]'> Content AI </span>
+                    <figure className='w-[40px] h-[40px] relative'>
+                        <Image fill src="svgs/logo.svg" alt="Content AI" />
+                    </figure>
+                    <h3 className='text-[18px] md:text-[20px] font-medium bg-gradient-to-tr from-blue-700 to-blue-400 bg-clip-text text-transparent'>
+                        Content AI
+                    </h3>
                 </Link>
-                <Link href={'/sign-up'} className='flex items-center gap-2 text-[15px] md:text-[18px] font-[400]'>
-                    <User className='w-[20px] md:w-[28px]' />
+                <Link href={'/sign-up'} className='flex items-center gap-2 text-[16px] md:text-[18px] font-[400]'>
+                    <User className='w-[23px] md:w-[28px]' />
                     Get Started
                 </Link>
             </div>
